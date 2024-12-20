ed761c537b9ed976fc57ba744f0de1e3e13e934f
lordTocho <brian.highnam@accenturefederaldefense.com>
2024-12-20 09:19:03 -0500

    bulding Restaurant stub

---
+++ src/main/java/com/codecademy/portfolio/diningReviewApi/models/Restaurant.java
@@ -0,0 +1,12 @@
+package com.codecademy.portfolio.diningReviewApi.models;
+
+public class Restaurant {
+    // id
+    // name
+    // user
+    // review
+    // peanutRating
+    // eggRating
+    // dairyRating
+    // averageRating( avg. peanut, egg, Dairy )
+}
