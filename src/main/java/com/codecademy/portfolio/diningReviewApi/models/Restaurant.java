2121aa1d15f93f4b374f03ff55482b3d3c818dbc
lordTocho <brian.highnam@accenturefederaldefense.com>
2025-01-02 14:00:16 -0500

    moved to using jakarta.persistence for java 23

--- src/main/java/com/codecademy/portfolio/diningReviewApi/models/Restaurant.java
+++ src/main/java/com/codecademy/portfolio/diningReviewApi/models/Restaurant.java
@@ -1,5 +1,13 @@
 package com.codecademy.portfolio.diningReviewApi.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+
+@Entity
+@Table(name = "RESTAURANTS")
 public class Restaurant {
     // id
     // name
@@ -9,4 +17,29 @@ public class Restaurant {
     // eggRating
     // dairyRating
     // averageRating( avg. peanut, egg, Dairy )
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(name = "NAME")
+    private String name;
+
+    @Column(name = "USER")
+    private String user;
+
+    @Column(name = "REVIEW")
+    private String review;
+
+    @Column(name = "PEANUTRATING")
+    private Integer peanutRating;
+
+    @Column(name = "EGGRATING")
+    private Integer eggRating;
+
+    @Column(name = "DAIRYRATING")
+    private Integer dairyRating;
+
+    @Column(name = "AVERAGERATING")
+    private double averageRating;
 }
