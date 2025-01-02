ef41ce2988fe32db455e0d932437e09f1a204c70
lordTocho <brian.highnam@accenturefederaldefense.com>
2025-01-02 14:32:50 -0500

    Added lombok to Resturaunt model

--- src/main/java/com/codecademy/portfolio/diningReviewApi/models/Restaurant.java
+++ src/main/java/com/codecademy/portfolio/diningReviewApi/models/Restaurant.java
@@ -6,6 +6,11 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 
+// auto generate setters and getters with lombok
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+
 @Entity
 @Table(name = "RESTAURANTS")
 public class Restaurant {
@@ -20,33 +25,43 @@ public class Restaurant {
 
     @Id
     @GeneratedValue
+    @Setter( AccessLevel.PUBLIC )
+    @Getter
     private Integer id;
 
+    @Setter( AccessLevel.PUBLIC )
+    @Getter
     @Column(name = "NAME")
     private String name;
 
+    @Setter( AccessLevel.PUBLIC )
+    @Getter
     @Column(name = "CUSTOMER")
     private String CUSTOMER;
 
+    @Setter( AccessLevel.PUBLIC )
+    @Getter
     @Column(name = "REVIEW")
     private String review;
 
+    @Setter( AccessLevel.PUBLIC )
+    @Getter
     @Column(name = "PEANUTRATING")
     private Integer peanutRating;
 
+    @Setter( AccessLevel.PUBLIC )
+    @Getter
     @Column(name = "EGGRATING")
     private Integer eggRating;
 
+    @Setter( AccessLevel.PUBLIC )
+    @Getter
     @Column(name = "DAIRYRATING")
     private Integer dairyRating;
 
+    @Setter( AccessLevel.PUBLIC )
+    @Getter
     @Column(name = "AVERAGERATING")
     private double averageRating;
 
-    public void setId( Integer id ) {
-        this.id = id;
-    }
-    public Integer getId() {
-        return this.id;
-    }
 }
